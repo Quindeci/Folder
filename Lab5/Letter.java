5475da8e9b27a9b6fb08490f523efff360575ee0
Quindeci <66300731+Quindeci@users.noreply.github.com>
2020-06-09 20:20:36 +0300

    Add files via upload

---
+++ Lab5/Letter.java
@@ -0,0 +1,9 @@
+package lab5;
+public class Letter {
+    private char lett;
+
+    Letter(char text) {
+        this.lett = text;
+
+    }
+}
---
+++ Lab5/Punctuation.java
@@ -0,0 +1,9 @@
+package lab5;
+public class Punctuation {
+    private char symbol;
+
+    Punctuation(char punct) {
+        this.symbol = punct;
+    }
+
+}
